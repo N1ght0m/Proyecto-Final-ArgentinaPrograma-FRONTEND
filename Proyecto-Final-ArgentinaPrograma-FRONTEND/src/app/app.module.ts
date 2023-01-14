f8068158c8cc9a5d8645a8333a0d33bd86e90bfc
N1ght0m <tomasgeorge79@gmail.com>
2023-01-14 18:32:20 -0300

    created components

--- Proyecto-Final-ArgentinaPrograma-FRONTEND/src/app/app.module.ts
+++ Proyecto-Final-ArgentinaPrograma-FRONTEND/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialMediaComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
