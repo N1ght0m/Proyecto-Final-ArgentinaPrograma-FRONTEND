723d361c2a8f2a9697913dfe70ec97773fd65407
N1ght0m <tomasgeorge79@gmail.com>
2023-01-15 01:38:11 -0300

    added about me and adjusted it

--- Proyecto-Final-ArgentinaPrograma-FRONTEND/src/app/app.module.ts
+++ Proyecto-Final-ArgentinaPrograma-FRONTEND/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialMediaComponent } from './components/social-media/social-media.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoAPComponent,
     SocialMediaComponent,
-    BannerComponent
+    BannerComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
