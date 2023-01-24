6a1d3c50958fb90c114cdcaa2b9b3ab4155730a5
N1ght0m <tomasgeorge79@gmail.com>
2023-01-24 18:26:27 -0300

    added  footer

--- Proyecto-Final-ArgentinaPrograma-FRONTEND/src/app/app.module.ts
+++ Proyecto-Final-ArgentinaPrograma-FRONTEND/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardSoftSkillsComponent } from './components/hard-soft-skills/hard-soft-skills.component';
 import { ProjectComponent } from './components/project/project.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { ProjectComponent } from './components/project/project.component';
     ExperienceComponent,
     EducacionComponent,
     HardSoftSkillsComponent,
-    ProjectComponent
+    ProjectComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
