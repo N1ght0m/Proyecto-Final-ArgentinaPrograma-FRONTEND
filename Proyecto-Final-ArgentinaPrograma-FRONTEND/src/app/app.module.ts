22110db521d35bac471adf53e89317b4a26152f2
N1ght0m <tomasgeorge79@gmail.com>
2023-01-24 20:33:34 -0300

    adjusted a little in the front end

--- Proyecto-Final-ArgentinaPrograma-FRONTEND/src/app/app.module.ts
+++ Proyecto-Final-ArgentinaPrograma-FRONTEND/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { SocialMediaComponent } from './components/social-media/social-media.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { ExperienceComponent } from './components/experience/experience.component';
@@ -20,7 +19,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    SocialMediaComponent,
     BannerComponent,
     AboutMeComponent,
     ExperienceComponent,
