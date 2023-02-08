d731ef170f3380fd9a3974f4492e308f49c3b5e9
N1ght0m <tomasgeorge79@gmail.com>
2023-02-08 20:55:42 -0300

    added login page

--- Proyecto-Final-ArgentinaPrograma-FRONTEND/src/app/app.module.ts
+++ Proyecto-Final-ArgentinaPrograma-FRONTEND/src/app/app.module.ts
@@ -13,6 +13,9 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardSoftSkillsComponent } from './components/hard-soft-skills/hard-soft-skills.component';
 import { ProjectComponent } from './components/project/project.component';
 import { FooterComponent } from './components/footer/footer.component';
+import {HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -25,12 +28,15 @@ import { FooterComponent } from './components/footer/footer.component';
     EducacionComponent,
     HardSoftSkillsComponent,
     ProjectComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
